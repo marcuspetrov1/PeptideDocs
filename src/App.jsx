d225c4334bcf8b25ef22167abfba74dda18ffa6d
marcuspetrov <197834194+marcuspetrovcsun@users.noreply.github.com>
2026-06-08 22:48:19 -0700

    feat: add ScrollToTop component to reset scroll position on route change

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from './pages/Home.jsx'
 import Catalog from './pages/Catalog.jsx'
 import PeptideDetail from './pages/PeptideDetail.jsx'
 import GetStarted from './pages/GetStarted.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 export default function App() {
   return (
     <Layout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<Catalog />} />
